ea9239af4fa02f4e426da8f4faeb0db7976b4b18
JorgeACL18 <jcanozolorenzo@danielcastelao.org>
2026-02-16 21:24:27 +0100

    Código terminado

--- addons/reportar/__manifest__.py
+++ addons/reportar/__manifest__.py
@@ -1,34 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "reportar",
+    'name': "Reportes",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Impresora de reportes en PDF",
 
-    'description': """
-Long description of module's purpose
-    """,
 
     'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'depends': ['base', 'sale'],
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'reports/sale_report.xml',
+        'reports/sale_report_action.xml',
+        'reports/sale_report_inherit.xml'
     ],
-    # only loaded in demonstration mode
+
     'demo': [
         'demo/demo.xml',
     ],
+
+    'installable': True,
+    'application': True,
 }
 
